671cc3b498e1b1d34e3193e976267bed09d324ff
AntonDevop <AntonDevop@gmail.com>
2018-09-20 02:04:50 +0600

    Product model added
    Modified model.py file of Product app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)
+    votes_total =  models.PositiveIntegerField(default = 1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
