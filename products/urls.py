1cd89aa8d31e1dada69af3955484521e9c78931e
AntonDevop <AntonDevop@gmail.com>
2018-09-22 23:39:12 +0600

    Detail page
    Added product's detail page with product id in address

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='detail'),
     # path('login/', views.login, name='login'),
     # path('logout/', views.logout, name='logout'),
 ]
